008eb0bbbf4bbcee87a5ae46779272bb643d165d
ElasticSea <info@elasticsea.com>
2019-06-03 01:13:04 +0200

    Player can jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,30 @@ public class Player : MonoBehaviour
 {
     public float forwardVelocity = 50;
     public float sidewaysVelocity = 10;
+    public float jumpForce = 1000;
     [SerializeField] private bool alive = true;
     
     void Awake()
     {
         // Move center of mass lower to ground, so the cube does not tumble
         GetComponent<Rigidbody>().centerOfMass = new Vector3(0,-.5f, 0);
+    }
+
+    private void Update()
+    {
+        if (alive == false) return;
+
+        var isTouchingGround = Physics.Linecast(transform.position , transform.position + Vector3.down * .51f);
+        
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+        }
+
+        // Ensure that cube does not rotate sideways
+        var rotation = transform.rotation.eulerAngles;
+        rotation.y = 0;
+        transform.rotation = Quaternion.Euler(rotation);
     }
 
     void FixedUpdate()
@@ -28,5 +46,6 @@ public class Player : MonoBehaviour
     public void Die()
     {
         alive = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
     }
 }
